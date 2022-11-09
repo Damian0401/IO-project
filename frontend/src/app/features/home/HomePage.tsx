bfab1b64b3c2afb42bd9c7f455048245e590cdcf
Damian0401 <szkola_abc@o2.pl>
2022-11-09 21:06:16 +0100

    Created base react project structure

---
+++ frontend/src/app/features/home/HomePage.tsx
@@ -0,0 +1,13 @@
+
+
+export default function HomePage() {
+
+
+    return (
+        <>
+            <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                Here will be the home page.
+            </div>
+        </>
+    )
+}

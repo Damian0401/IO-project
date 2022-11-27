1458cd1eda15f8a61f1e0971e8f4b257a131f398
Damian0401 <szkola_abc@o2.pl>
2022-11-27 20:10:13 +0100

    Added migrations and script to generate database

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- backend/Persistence/DataContext.cs
+++ backend/Persistence/DataContext.cs
@@ -5,6 +5,12 @@ namespace Persistence;
 
 public class DataContext : DbContext
 {
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+        
+    }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.Entity<UserData>()
